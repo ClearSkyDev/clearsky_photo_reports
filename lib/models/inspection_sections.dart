7190f9f496383ec6c05aa10427438f8724fd8124
ClearSkyDev <psalms638@gmail.com>
2025-06-15 13:51:08 -0400

    Implement structured photo upload workflow

---
+++ lib/models/inspection_sections.dart
@@ -0,0 +1,14 @@
+const List<String> kInspectionSections = [
+  'Address Photo',
+  'Front of House',
+  'Front Elevation & Accessories',
+  'Right Elevation & Accessories',
+  'Back Elevation & Accessories',
+  'Backyard Damages',
+  'Left Elevation & Accessories',
+  'Roof Edge',
+  'Roof Slopes - Front',
+  'Roof Slopes - Right',
+  'Roof Slopes - Back',
+  'Roof Slopes - Left',
+];
