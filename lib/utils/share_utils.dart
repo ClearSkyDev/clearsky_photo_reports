539feb0a3af799fc588506cdca0e5b42e620d9cb
ClearSkyDev <psalms638@gmail.com>
2025-06-19 19:45:58 -0400

    refactor: update share utils

--- lib/utils/share_utils.dart
+++ lib/utils/share_utils.dart
@@ -1,27 +1,16 @@
 import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:share_plus/share_plus.dart';
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
 
 /// Shares [reportFile] using the native share sheet when available.
-///
-/// On web, the file is downloaded instead since a share sheet is not
-/// supported in most browsers.
 Future<void> shareReportFile(File reportFile,
     {String? subject, String? text}) async {
-  if (kIsWeb) {
-    final bytes = await reportFile.readAsBytes();
-    final blob = html.Blob([bytes]);
-    final url = html.Url.createObjectUrlFromBlob(blob);
-    final anchor = html.AnchorElement(href: url)
-      ..setAttribute('download', reportFile.path.split('/').last)
-      ..click();
-    html.Url.revokeObjectUrl(url);
-    return;
-  }
   try {
-    await SharePlus.instance.shareXFiles([XFile(reportFile.path)],
+    await Share.shareXFiles([XFile(reportFile.path)],
         subject: subject, text: text);
   } catch (_) {}
+}
+
+Future<void> shareFiles(List<String> filePaths, {String? text}) async {
+  final files = filePaths.map((path) => XFile(path)).toList();
+  await Share.shareXFiles(files, text: text);
 }
