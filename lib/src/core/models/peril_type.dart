837b479b93ac9e71bf6d986647d8fc0a69f63983
ClearSkyDev <psalms638@gmail.com>
2025-06-20 15:32:07 -0400

    Add missing license and basic CI; implement PerilType model and invoice detail dialog

--- lib/src/core/models/peril_type.dart
+++ lib/src/core/models/peril_type.dart
@@ -1,1 +1,22 @@
-// TODO Implement this library.
+/// Types of perils an insurance claim may cover.
+enum PerilType {
+  wind,
+  hail,
+  fire,
+  flood,
+}
+
+extension PerilTypeDisplay on PerilType {
+  String get displayName {
+    switch (this) {
+      case PerilType.wind:
+        return 'Wind';
+      case PerilType.hail:
+        return 'Hail';
+      case PerilType.fire:
+        return 'Fire';
+      case PerilType.flood:
+        return 'Flood';
+    }
+  }
+}
