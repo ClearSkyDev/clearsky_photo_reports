ccb845bb1c9f23d0a23458e9fdc1c91076561c94
ClearSkyDev <psalms638@gmail.com>
2025-06-22 13:14:28 -0400

    feat: store external report urls

---
+++ test/core/models/inspection_metadata_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:clearsky_photo_reports/src/core/models/inspection_metadata.dart';
+import 'package:clearsky_photo_reports/src/core/models/inspection_type.dart';
+import 'package:clearsky_photo_reports/src/core/models/peril_type.dart';
+import 'package:clearsky_photo_reports/src/core/models/inspector_report_role.dart';
+
+void main() {
+  test('external report urls round trip', () {
+    final metadata = InspectionMetadata(
+      clientName: 'C',
+      propertyAddress: 'A',
+      inspectionDate: DateTime(2024, 1, 1),
+      perilType: PerilType.wind,
+      inspectionType: InspectionType.residentialRoof,
+      inspectorRoles: {InspectorReportRole.ladderAssist},
+      externalReportUrls: ['u1', 'u2'],
+    );
+    final map = metadata.toMap();
+    final copy = InspectionMetadata.fromMap({
+      ...map,
+      'inspectionDate': metadata.inspectionDate.toIso8601String(),
+    });
+    expect(copy.externalReportUrls, ['u1', 'u2']);
+  });
+}
