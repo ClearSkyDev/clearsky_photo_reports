069154356e5138d3ebe0ba8f3ad2e450a27c4083
ClearSkyDev <psalms638@gmail.com>
2025-07-16 09:57:24 -0400

    fix: disable Flipper in Podfile patch

--- scripts/disable_flipper.py
+++ scripts/disable_flipper.py
@@ -18,9 +18,13 @@ else:
 
     new_lines = []
     for line in lines:
-        # Comment out any Flipper-related lines entirely to avoid referencing
-        # the FlipperConfiguration constant which may not be defined.
-        if "FlipperConfiguration" in line or "use_flipper!" in line:
+        # Replace any Flipper configuration with an empty hash to fully disable
+        # it while avoiding undefined constant errors.
+        if "FlipperConfiguration" in line or "flipper_config" in line:
+            new_lines.append("flipper_config = {}\n")
+        # Comment out any call to `use_flipper!` so CocoaPods skips Flipper
+        # integration entirely.
+        elif "use_flipper!" in line:
             new_lines.append("# " + line)
         else:
             new_lines.append(line)
