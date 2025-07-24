88c4baabd446c877df94d03e0c5805049ee7c30e
ClearSkyDev <psalms638@gmail.com>
2025-07-24 18:06:02 -0400

    Fix React Native entry with stack navigation

--- react_native/MainApp.js
+++ react_native/MainApp.js
@@ -6,7 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 // Screens
 import SplashScreen from './screens/SplashScreen';
 import PhotoIntakeScreen from './screens/PhotoIntakeScreen';
-import ReportPreviewScreen from './ReportPreviewScreen';
+import ReportPreviewScreen from './screens/ReportPreviewScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,8 +14,12 @@ export default function MainApp() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Navigator initialRouteName="Splash">
+          <Stack.Screen
+            name="Splash"
+            component={SplashScreen}
+            options={{ headerShown: false }}
+          />
           <Stack.Screen name="PhotoIntake" component={PhotoIntakeScreen} />
           <Stack.Screen name="ReportPreview" component={ReportPreviewScreen} />
         </Stack.Navigator>
