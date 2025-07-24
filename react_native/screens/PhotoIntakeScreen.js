622539c78dda848385a87f5542e86562231d5197
ClearSkyDev <psalms638@gmail.com>
2025-07-24 07:23:47 -0400

    Implement code changes to enhance functionality and improve performance

--- react_native/screens/PhotoIntakeScreen.js
+++ react_native/screens/PhotoIntakeScreen.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 export default function PhotoIntakeScreen() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Text style={{ fontSize: 20, textAlign: 'center' }}>Photo Intake Screen</Text>
-      {/* Add temporary dummy photo tiles here */}
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Photo Intake Screen</Text>
+      <Button title="Click Me" onPress={() => alert("It works!")} />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
